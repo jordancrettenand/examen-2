be2db44feb8daf76efb2be42289e4eb789dcf4ec
jordancrettenand <122967586+jordancrettenand@users.noreply.github.com>
2023-12-11 17:07:23 +0100

    Finalisation

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var burger = document.querySelector(".burger");
+var menu = document.querySelector(".menu");
+var toggleMenu = function toggleMenu() {
+  menu.classList.toggle("is-active");
+};
+burger.addEventListener("click", toggleMenu);
